195286f8d766a5a9d1bbc3c57f7ba3c9c92b9b50
amls <amirhoseinbr83@gmail.com>
2024-06-11 15:21:39 +0330

    first version of compelted project

---
+++ study_club/base/api/views.py
@@ -0,0 +1,21 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from base.models import Room
+from .serializers import Room_serializer
+
+@api_view(['GET'])
+def get_routes(request):
+    routes = ["GET /api","GET /api/rooms", "GET /api/rooms/:id"]
+    return Response(routes)
+
+@api_view(['GET'])
+def get_rooms(request):
+    rooms = Room.objects.all()
+    serializer = Room_serializer(rooms,many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_room(request, pk):
+    room = Room.objects.get(id=pk)
+    serializer = Room_serializer(room,many=False)
+    return Response(serializer.data)
